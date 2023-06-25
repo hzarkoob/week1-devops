82c889d67b0a26934112076b4b755d06e3628fec
Hadi Zarkoob <hadi.zarkoob114@gmail.com>
2023-06-25 00:15:04 +0000

    New fail

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@ from calculator import add, div, mul, sub
 
 
 def test_add():
-    assert add(1, 1) == 3
+    assert add(1, 1) == 2
 
 
 def test_sub():
@@ -15,3 +15,6 @@ def test_mul():
 
 def test_div():
     assert div(2, 1) == 2
+
+def test_add2():
+    assert add(1, 1) == 3    
